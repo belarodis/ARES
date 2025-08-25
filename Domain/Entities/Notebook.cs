a7189c6addaa2e6aa7fa66ae1cf85281d5cdba8e
Marcela Nunes Zarichta <marcela.zarichta@edu.pucrs.br>
2025-08-25 18:06:32 -0300

    Entidades criadas

---
+++ Domain/Entities/Notebook.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class Notebook
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(15)]
+    public string NPatrimonio { get; set; }
+
+    [Required]
+    public DateOnly DataAquisicao { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Descricao { get; set; }
+
+    // Navigation properties
+    public ICollection<ReservaNotebook> Reservas { get; set; }
+}
