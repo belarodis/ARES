5a6bc0ab3d3cb0b9f1fb64f5dcfcb62cbbc46482
Henrique-Schultz <valente.schultz@gmail.com>
2025-08-27 14:12:03 -0300

    base front completa

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/app-header.component';
+import { Calendario } from "./shared/calendario/calendario";
+import { Gerenciar } from "./shared/gerenciar/gerenciar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, Calendario, Gerenciar],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
   standalone: true,
