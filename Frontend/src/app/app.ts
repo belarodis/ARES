665fa3b508f9bb9c2d195ad37e69009974a5c89f
Henrique-Schultz <valente.schultz@gmail.com>
2025-08-28 16:50:51 -0300

    button-novo-recurso

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './shared/header/app-header.component';
 import { CalendarioComponent } from './shared/calendario/calendario.component';
 import { Gerenciar } from './shared/gerenciar/gerenciar';
 import { Filtro } from "./shared/filtro/filtro";
+import { ButtonNovoRecurso } from "./shared/button-novo-recurso/button-novo-recurso";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CalendarioComponent, Gerenciar, Filtro],
+  imports: [RouterOutlet, HeaderComponent, CalendarioComponent, Gerenciar, Filtro, ButtonNovoRecurso],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
   standalone: true,
