4d20c02ce855001c2f3cc56818c30f7c60ff7b33
Isabela Rodrigues de Souza <bela.rodjj@gmail.com>
2025-08-28 19:33:25 -0300

    aaaaaaaa

--- Data/Migrations/20250828223209_Seeds.Designer.cs
+++ Data/Migrations/20250828223209_Seeds.Designer.cs
@@ -4,6 +4,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(ARESDbContext))]
-    partial class ARESDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250828223209_Seeds")]
+    partial class Seeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
